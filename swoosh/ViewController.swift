e1ecebcbc9fa54408a29feb5a7274d56bf8758a4
Wellington Cabral da Silva <wellingtoncab@gmail.com>
2017-11-07 17:12:32 -0300

    first commit

---
+++ swoosh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swoosh
+//
+//  Created by Wellington Cabral da Silva on 31/10/17.
+//  Copyright © 2017 Wellington Cabral da Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
